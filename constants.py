cca4bcb475e8e448df574245e09c8931d6eed94a
Jaime Castro <jecastro.ie@gmail.com>
2018-08-15 02:49:21 -0300

    Improvements for debugging

--- constants.py
+++ constants.py
@@ -1,5 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
+if os.path.exists('.env'):
+    load_dotenv(dotenv_path='.env')
+
 GITHUB_SECRET = os.environ.get('GITHUB_SECRET')
 SERVER_URL = os.environ.get('SERVER_URL')
 TELEGRAM_ID = os.environ.get('TELEGRAM_ID')
